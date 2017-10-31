b3b8be94b56738a1c1974e8967a7ede19d36a93d
alexmitroff <alexmitroff@gmail.com>
2017-10-31 13:30:06 +0300

    Added first models

--- experiment/models.py
+++ experiment/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class UserInfo(models.Model):
+    GENDER_CHOISES = (
+            (0, u"Мужской"),
+            (1, u"Женский"),
+            )
+
+    lastname = models.CharField( u"фамилия", max_length=50)
+    firstname
+    gender = models.IntegerField( u"пол", default=0, choices=GENDER_CHOISES )
+    age
+    edu_type
+    edu
+    art_exp
+    edu_prof
+    email
+    phone
+    cinema_freq
+    cinema_genre
+
+    class Meta:
+
+    def __str__(self):
+        return "%s %s" % (self.lastname, self.firstname)
+
