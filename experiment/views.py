e7e2c797933f32220edb717678e922a9f0b3e1a0
alexmitroff <alexmitroff@gmail.com>
2017-11-21 13:53:23 +0300

    Pushed data to tamplate

--- experiment/views.py
+++ experiment/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from experiment.forms import UserInfoForm
 
+from experiment.models import *
+
 # Create your views here.
 
 def index(request):
     template = "pages/index.html"
     form = UserInfoForm()
+    stimul = Stimul.objects.filter(show=True)[0]
+    images = Image.objects.filter(stimul = stimul)
     var = {
             'form':form,
-            'img_count':5,
+            'stimul':stimul,
+            'images':images,
             }
     return render(request, template, var)
